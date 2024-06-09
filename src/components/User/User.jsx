55fc44d3ef00e88f7f5424df6d7a501d4f95b093
TechRam09 <nsshreeram@gmail.com>
2024-06-10 01:21:01 +0530

    Update User.jsx
    Added styles

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom"
 function User() {
     const {userid} = useParams()
   return (
-      <div className="text-center">User:{userid}</div>
+      <div className="text-center bg-orange-700 text-white text-4xl p-5 m-12">User:{userid}</div>
   )
 }
 
